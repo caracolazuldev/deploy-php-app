b48d907c9b946c4905917ab3c6a05458978eaafb
ginkgoMZD <mzd@ginkgostreet.com>
2022-04-13 13:34:39 -0400

    fix and refactor;

--- src/dump-tables.mk
+++ src/dump-tables.mk
@@ -6,21 +6,29 @@
 # - DATABASE
 # - MYSQL_CLI
 
-MYSQL_DUMP = $(subst mysql,mysqldump,${MYSQL_CLI})
+SEARCH_SQL_GTID := ^SET @@GLOBAL\.GTID_PURGED.*$$
+SEARCH_SQL_TEMP_LOGBIN := ^SET @MYSQLDUMP_TEMP_LOG_BIN.*$$
+SEARCH_SQL_LOGBIN := ^SET @@SESSION.SQL_LOG_BIN.*$$
+EXCLUDED_CACHE_TABLES := civicrm_acl_cache civicrm_acl_contact_cache civicrm_cache civicrm_group_contact_cache
+DEFINER_REGEX := /\*![0-9]* DEFINER=[^*]*\*/ # includes trailing space; fwd-slashes are part of pattern;
 
-rm-definer = sed 's%/\*![0-9]* DEFINER=[^*]*/%%'
+mysql-dump = $(subst mysql,mysqldump,${MYSQL_CLI})
 
+remove-in = sed 's%$1%%g'
 define dump-table
-        ${MYSQL_DUMP} --skip-comments --skip-dump-date ${DATABASE} $1 | ${rm-definer} >> $@
+    ${mysql-dump} --skip-comments --skip-dump-date ${DATABASE} $1 \
+		| $(call remove-in,${DEFINER_REGEX}) \
+		| $(call remove-in,${SEARCH_SQL_LOGBIN}) \
+		| $(call remove-in,${SEARCH_SQL_TEMP_LOGBIN}) \
+		| $(call remove-in,${SEARCH_SQL_GTID}) \
+		>> $@
 
 endef
 
-civi-tables := $(shell echo "show tables like 'civicrm_%';" | ${MYSQL_CLI} --skip-column-names ${DATABASE})
-wp-tables := $(shell echo "show tables like 'wp_%';" | ${MYSQL_CLI} --skip-column-names ${DATABASE})
+grep-excludes := $(foreach e,${EXCLUDED_CACHE_TABLES}, -e $e)
+tables-matching = echo "show tables like '$1';" | ${MYSQL_CLI} --skip-column-names ${DATABASE}
 
-exclude-cache-tables := civicrm_acl_cache civicrm_acl_contact_cache civicrm_cache civicrm_group_contact_cache
-grep-excludes := $(foreach e,${exclude-cache-tables}, -e $e)
-table-list := $(shell echo "${civi-tables}\n${wp-tables}" | grep -v ${grep-excludes})
+table-list := $(shell $(call tables-matching,civicrm_%) | grep -v ${grep-excludes}) $(shell $(call tables-matching,wp_%) | grep -v ${grep-excludes})
 
 arch/${DATABASE}.sql:
-	$(foreach tbl,${table-list},$(call dump-table,${tbl}))
+	$(foreach tbl,${table-list},$(info ${tbl}) $(call dump-table,${tbl}))
